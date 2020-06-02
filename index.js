12c0aa723cdb56481eee987baa8ef4d0ee8a7930
M Thariq Nugroho <thariq1902@gmail.com>
2020-06-02 14:14:29 +0700

    update index.js

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ const PORT = process.env.PORT || 3000;
 const TOKEN = require("./tokenProvider");
 
 const generateAccessToken = (username) => {
-  // expires after half and hour (1800 seconds = 30 minutes)
+  // expires after a hour
   return jwt.sign(username, TOKEN, { expiresIn: "3600s" });
 };
 
 const authenticateToken = (req, res, next) => {
-  // Gather the jwt access token from the request header
+  // Gather the jwt access token from the request params
   const authHeader = req.params.token;
-  // console.log(authHeader);
-  // const token = authHeader && authHeader.split(' ')[1]
   if (authHeader == null) return res.sendStatus(401); // if there isn't any token
 
   jwt.verify(authHeader, TOKEN, (error, user) => {
@@ -25,14 +23,12 @@ const authenticateToken = (req, res, next) => {
 };
 
 app.get("/:username", (req, res) => {
-  // res.send("process.env.TOKEN_SECRET", process.env.TOKEN_SECRET);
   const { username } = req.params;
   const token = generateAccessToken({ username });
   res.json({ TOKEN_SECRET: token });
 });
 
 app.get("/login/:username/:token", authenticateToken, (req, res) => {
-  // console.log("kenapa ", req.user);
   res.json(req.user);
 });
 
